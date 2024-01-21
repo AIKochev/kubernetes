2e1e4601836249571d1b31e79c9fae50b95da246
Alex Kochev <alekskochev@Alekss-MacBook-Pro.local>
2024-01-21 20:15:24 +0200

    Adding users-api deployment

--- networking/users-api/users-app.js
+++ networking/users-api/users-app.js
@@ -11,19 +11,17 @@ app.post('/signup', async (req, res) => {
   const email = req.body.email;
   const password = req.body.password;
 
-  if (
-    !password ||
-    password.trim().length === 0 ||
-    !email ||
-    email.trim().length === 0
-  ) {
-    return res
+  if (!password || password.trim().length === 0 
+      || !email || email.trim().length === 0) {
+    
+      return res
       .status(422)
       .json({ message: 'An email and password needs to be specified!' });
   }
 
   try {
-    const hashedPW = await axios.get('http://auth/hashed-password/' + password);
+    // const hashedPW = 'dummy pass'
+    const hashedPW = await axios.get(`http://${process.env.AUTH_ADDRESS}/hashed-password/` + password);
     // since it's a dummy service, we don't really care for the hashed-pw either
     console.log(hashedPW, email);
     res.status(201).json({ message: 'User created!' });
@@ -40,11 +38,8 @@ app.post('/login', async (req, res) => {
   const email = req.body.email;
   const password = req.body.password;
 
-  if (
-    !password ||
-    password.trim().length === 0 ||
-    !email ||
-    email.trim().length === 0
+  if ( !password || password.trim().length === 0 
+      || !email || email.trim().length === 0
   ) {
     return res
       .status(422)
@@ -53,8 +48,9 @@ app.post('/login', async (req, res) => {
 
   // normally, we'd find a user by email and grab his/ her ID and hashed password
   const hashedPassword = password + '_hash';
+  //  const response = { status: 200, data: { token: 'abc' } };
   const response = await axios.get(
-    'http://auth/token/' + hashedPassword + '/' + password
+    `http://${process.env.AUTH_ADDRESS}/token/` + hashedPassword + '/' + password
   );
   if (response.status === 200) {
     return res.status(200).json({ token: response.data.token });
