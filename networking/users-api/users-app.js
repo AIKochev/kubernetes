b66488c1f3524d4f38e40656670047e9aed4dc2a
Alex Kochev <alekskochev@Alekss-MacBook-Pro.local>
2024-01-23 22:40:09 +0200

    minor changes

--- networking/users-api/users-app.js
+++ networking/users-api/users-app.js
@@ -20,7 +20,6 @@ app.post('/signup', async (req, res) => {
   }
 
   try {
-    // const hashedPW = 'dummy pass'
     const hashedPW = await axios.get(`http://${process.env.AUTH_ADDRESS}/hashed-password/` + password);
     // since it's a dummy service, we don't really care for the hashed-pw either
     console.log(hashedPW, email);
@@ -48,9 +47,8 @@ app.post('/login', async (req, res) => {
 
   // normally, we'd find a user by email and grab his/ her ID and hashed password
   const hashedPassword = password + '_hash';
-  //  const response = { status: 200, data: { token: 'abc' } };
   const response = await axios.get(
-    `http://${process.env.AUTH_ADDRESS}/token/` + hashedPassword + '/' + password
+    `http://${process.env.AUTH_SERVICE_SERVICE_HOST}/token/` + hashedPassword + '/' + password
   );
   if (response.status === 200) {
     return res.status(200).json({ token: response.data.token });
